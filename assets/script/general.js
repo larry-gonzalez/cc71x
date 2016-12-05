0e1e3c63af8cc03d861fea50e135189af4cbea50
larryjgonzalez <larry@hit-map.com>
2016-12-05 11:50:06 -0300

    start to generate an interpreter

--- assets/script/general.js
+++ assets/script/general.js
@@ -1,3 +1,5 @@
+var temp = ''
+var temp2 = ''
 activate_rdfdat = function(){
     $('#sparql_item').removeClass('active');
     $('#rdfdat_item').addClass('active');
@@ -11,3 +13,27 @@ activate_sparql = function(){
     $('#rdfdat_div').addClass('invisible');
 }
 
+
+load_triples = function(){
+    temp = $('#turtle_data').val()
+}
+
+run_query = function(){}
+
+is_not_comment = function(string){
+    return string[0] != '#'
+}
+
+spo = function(string){
+    aux = string.split(' ')
+    if (aux.length != 4){
+        alert('bad syntax:', string)
+    }
+}
+
+
+simple_interp = function(string){
+    los = string.split('\n')
+    los = los.filter(is_not_comment)
+    temp2 = los
+}
