eaa5cbff78c1b136e991525bd0d2fc5407b05604
larryjgonzalez <larry@hit-map.com>
2016-12-05 22:42:59 -0300

    nodes are roundrectangle, have 0.5em padding, with width by label. Add better examples

--- cytoscape-example.js
+++ cytoscape-example.js
@@ -5,23 +5,23 @@ var cy = cytoscape({
   container: $('#cy'), // container to render in
 
   elements: [ // list of graph elements to start with
-    { data: { id: 'a', color:'blue' } },
-    { data: { id: 'b', color:'blue' } },
+    { data: { id: 'abanico', color:'blue' } },
+    { data: { id: 'barco', color:'blue' } },
     { data: { id: 'c', color:'yellow' } },
     { data: { id: 'd', color:'yellow' } },
     { data: { id: 'e', color:'gray' } },
     { data: { id: 'f', color:'gray' } },
     { data: { id: 'g', color:'green' } },
     { data: { id: 'h', color:'green' } },
-    { data: { id: 'i', color:'green' } },
-    { data: { id: 'ab', source: 'a', target: 'b', label: 'uno'} },
-    { data: { id: 'ac', source: 'a', target: 'c', label: 'edge'} },
-    { data: { id: 'ad', source: 'a', target: 'd', label: 'arista'} },
-    { data: { id: 'ae', source: 'a', target: 'e', label: 'enlace'} },
-    { data: { id: 'af', source: 'a', target: 'f', label: 'test'} },
+    { data: { id: 'indeterminadamente', color:'green' } },
+    { data: { id: 'ab', source: 'abanico', target: 'barco', label: 'uno'} },
+    { data: { id: 'ac', source: 'abanico', target: 'c', label: 'edge'} },
+    { data: { id: 'ad', source: 'abanico', target: 'd', label: 'arista'} },
+    { data: { id: 'ae', source: 'abanico', target: 'e', label: 'enlace'} },
+    { data: { id: 'af', source: 'abanico', target: 'f', label: 'test'} },
     { data: { id: 'fg', source: 'f', target: 'g', label: 'dos'} },
     { data: { id: 'fh', source: 'f', target: 'h', label: 'tres'} },
-    { data: { id: 'fi', source: 'f', target: 'i', label: 'cuatro'} }
+    { data: { id: 'fi', source: 'f', target: 'indeterminadamente', label: 'cuatro'} }
 
   ],
 
@@ -31,8 +31,12 @@ var cy = cytoscape({
       style: {
         'background-color': 'data(color)',
         'label': 'data(id)',
+        'width': 'label',
+        'shape': 'roundrectangle',
         'text-valign': 'center',
         'text-halign': 'center',
+        'padding-left': '0.5em',
+        'padding-right': '0.5em'
       }
     },
 
